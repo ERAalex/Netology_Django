48fdc13fc5da8b14e310c9b7084203f664e695cb
Alex_ERA <eraspb@mail.ru>
2022-12-25 01:45:27 +0300

    done first task with request get <>

--- BIG_dj-homeworks-video/1.2-requests-templates/recipes/calculator/views.py
+++ BIG_dj-homeworks-video/1.2-requests-templates/recipes/calculator/views.py
@@ -1,30 +1,59 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-DATA = {
-    'omlet': {
+
+def index(request):
+    return render(request, 'calculator/index.html')
+
+
+def dish_show(request, name):
+    say_total_person = 'Рецепт блюда на 1 человека'
+
+    if name == 'omlet':
+        context = {
         'яйца, шт': 2,
         'молоко, л': 0.1,
         'соль, ч.л.': 0.5,
-    },
-    'pasta': {
+    }
+    elif name == 'pasta':
+        context = {
         'макароны, г': 0.3,
         'сыр, г': 0.05,
-    },
-    'buter': {
+    }
+    elif name == 'buter':
+        context = {
         'хлеб, ломтик': 1,
         'колбаса, ломтик': 1,
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
-    },
-    # можете добавить свои рецепты ;)
-}
+    }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+    return render(request, 'calculator/index.html', {'recipe':context, 'total_p':say_total_person})
+
+
+
+def dish_show_total(request, name, servings):
+    say_total_person = f'Рецепт блюда на {servings} чел.'
+
+    if name == 'omlet':
+        context = {
+        'яйца, шт': 2*int(servings),
+        'молоко, л': 0.1*int(servings),
+        'соль, ч.л.': 0.5*int(servings),
+    }
+    elif name == 'pasta':
+        context = {
+        'макароны, г': 0.3*int(servings),
+        'сыр, г': 0.05*int(servings),
+    }
+    elif name == 'buter':
+        context = {
+        'хлеб, ломтик': 1*int(servings),
+        'колбаса, ломтик': 1*int(servings),
+        'сыр, ломтик': 1*int(servings),
+        'помидор, ломтик': 1*int(servings),
+    }
+
+    return render(request, 'calculator/index.html', {'recipe':context, 'total_p':say_total_person})
+
+
--- BIG_dj-homeworks-video/1.2-requests-templates/recipes/recipes/urls.py
+++ BIG_dj-homeworks-video/1.2-requests-templates/recipes/recipes/urls.py
@@ -1,21 +1,12 @@
-"""recipes URL Configuration
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from calculator.views import dish_show, dish_show_total, index
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.urls import path
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('admin/', admin.site.urls),
+    path('<name>/<servings>', dish_show_total),
+    path('<name>/', dish_show),
+    path('', index),
 ]
